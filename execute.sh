1eec2719e314ec53134c593bb4c5c3e646e529c2
Aleksey Morshnev <morshnev.aleksey@gmail.com>
2024-07-29 15:45:59 +0200

    corrected sh and sub

--- execute.sh
+++ execute.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Path to the `.py` file you want to run
-PYTHON_SCRIPT_PATH="/home/hlcv_team030/team030/im2latex/"
+PYTHON_SCRIPT_PATH="/home/hlcv_team030/im2latex/"
 # Path to the Python binary of the conda environment
-CONDA_PYTHON_BINARY_PATH="/home/hlcv_team030/team030/miniconda3/envs/hlcv-ss23/bin/python"
+CONDA_PYTHON_BINARY_PATH="/home/hlcv_team030/miniconda3/envs/hlcv-ss23/bin/python"
 # Path to torchrun binary
-TORCHRUN_PATH="/home/hlcv_team030/team030/miniconda3/envs/hlcv-ss23/bin/torchrun"
+TORCHRUN_PATH="/home/hlcv_team030/miniconda3/envs/hlcv-ss23/bin/torchrun"
 
 cd $PYTHON_SCRIPT_PATH
 $TORCHRUN_PATH --nproc_per_node=5 "$@"
